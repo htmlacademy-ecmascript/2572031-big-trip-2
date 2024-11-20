f7fd8aca8df691eca8c5974e5bab079a5fcd4555
Maksim Roi <klinarr@yandex.ru>
2024-11-20 17:26:25 +0300

    .

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import FilterView from './view/filter-view.js';
 import SortView from './view/sort-view.js';
 import EventView from './view/events-view.js';
+import InfoTemplate from './view/info-view.js';
 import { render } from './render.js';
 
+const siteTripMainElement = document.querySelector('.trip-main');
 const siteFiltersElement = document.querySelector('.trip-controls__filters');
 const siteTripEventsElement = document.querySelector('.trip-events');
 
+render (new InfoTemplate(), siteTripMainElement);
 render(new FilterView(), siteFiltersElement);
 render(new SortView(), siteTripEventsElement);
 render(new EventView(), siteTripEventsElement);
