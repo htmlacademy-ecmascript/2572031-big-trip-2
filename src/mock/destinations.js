e54d4c66c0a91560c5b3c9e352457b4691d20248
Maksim Roi <klinarr@yandex.ru>
2024-12-18 16:41:41 +0300

    .

--- src/mock/destinations.js
+++ src/mock/destinations.js
@@ -4,22 +4,6 @@ const mockDestinations = [
     description: 'Chamonix, is a beautiful city, a true asian pearl, with crowded streets.',
     name: 'Chamonix',
     pictures: [
-      {
-        src: 'https://random.onl/images/pictures/np31.jpg',
-        description: 'Chamonix parliament building'
-      },
-      {
-        src: 'https://random.onl/images/pictures/np31.jpg',
-        description: 'Chamonix parliament building'
-      },
-      {
-        src: 'https://random.onl/images/pictures/np31.jpg',
-        description: 'Chamonix parliament building'
-      },
-      {
-        src: 'https://random.onl/images/pictures/np31.jpg',
-        description: 'Chamonix parliament building'
-      }
     ]
   },
   {
@@ -27,14 +11,6 @@ const mockDestinations = [
     description: 'Moscow, is a beautiful city, a true asian pearl, with crowded streets.',
     name: 'Moscow',
     pictures: [
-      {
-        src: 'https://via.assets.so/game.jpg?w=1280&h=720',
-        description: 'Moscow parliament building'
-      },
-      {
-        src: 'https://via.assets.so/game.jpg?w=1280&h=720',
-        description: 'Moscow parliament building'
-      },
     ]
   },
   {
