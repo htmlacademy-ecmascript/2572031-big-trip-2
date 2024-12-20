71d3e011d4b8c52e6451408763bc972772662d62
Maksim Roi <klinarr@yandex.ru>
2024-12-20 14:04:01 +0300

    .

--- src/view/events-edit-view.js
+++ src/view/events-edit-view.js
@@ -47,7 +47,7 @@ function createEventsEditTemplate(point, destination, offers , allDestinations) 
                         </div>
 
                         <div class="event__type-item">
-                          <input id="event-type-flight-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="flight" checked>
+                          <input id="event-type-flight-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="flight">
                           <label class="event__type-label  event__type-label--flight" for="event-type-flight-1">Flight</label>
                         </div>
 
